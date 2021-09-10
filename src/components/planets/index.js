97fe5f2f182c980241cee827c967959d9c8d6be4
Nicholas Velten <nikvelten@gmail.com>
2021-09-10 09:51:52 -0300

    Adiçao do fetch API

--- src/components/planets/index.js
+++ src/components/planets/index.js
@@ -1,26 +1,44 @@
 import React, { Fragment } from "react";
 import Planet from "./planet";
 
-const Planets = () => {
-  return (
-    <Fragment>
-      <h3>Planet List</h3>
-      <hr />
-      <Planet
-        name="Mercurio"
-        description="Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres."
-        img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
-        link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-      />
-      <hr />
-      <Planet
-        name="Pluto"
-        description="Plutão, formalmente designado 134340 Plutão, é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol."
-        img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
-        link="https://pt.wikipedia.org/wiki/Plut%C3%A3o"
-      />
-    </Fragment>
-  );
-};
+async function getPlanets() {
+  let response = await fetch("http://localhost:3000/api/planets.json");
+  let data = await response.json();
+  return data;
+}
+class Planets extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      planets: [],
+    };
+  }
+
+  componentDidMount() {
+    getPlanets().then((data) => {
+      this.setState((state) => ({
+        planets: data["planets"],
+      }));
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h3>Planet List</h3>
+        <hr />
+        {this.state.planets.map((planet) => (
+          <Planet
+            name={planet.name}
+            description={planet.description}
+            img_url={planet.img_url}
+            link={planet.link}
+            id={planet.id}
+          />
+        ))}
+      </Fragment>
+    );
+  }
+}
 
 export default Planets;
--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -1,22 +1,47 @@
 import React, { Fragment } from "react";
 import GrayImg from "../../shared/gray-img";
 import DescLink from "../../shared/link-desc";
-import Satellites from "../../shared/satellites";
 
-const Planet = (props) => {
-  return (
-    <Fragment>
-      <h4>{props.name}</h4>
-      <GrayImg img_url={props.img_url} />
-      <DescLink
-        desc_url={props.desc}
-        description={props.description}
-        link={props.link}
-      />
-      <h4>Satelites</h4>
-      {Satellites}
-    </Fragment>
-  );
-};
+async function getSatellites(id) {
+  let response = await fetch(`http://localhost:3000/api/${id}.json`);
+  let data = await response.json();
+  return data;
+}
 
+class Planet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      satellites: [],
+    };
+  }
+
+  componentDidMount() {
+    getSatellites(this.props.id).then((data) => {
+      this.setState((state) => ({
+        satellites: data["satellites"],
+      }));
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h4>{this.props.name}</h4>
+        <GrayImg img_url={this.props.img_url} />
+        <DescLink
+          desc_url={this.props.desc}
+          description={this.props.description}
+          link={this.props.link}
+        />
+        <h4>Satelites</h4>
+        <ul>
+          {this.state.satellites.map((satellites, index) => (
+            <li key={index}>{satellites.name}</li>
+          ))}
+        </ul>
+      </Fragment>
+    );
+  }
+}
 export default Planet;
